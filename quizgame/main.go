1bc428981e1288dce50516fe18d5257b68856d06
René Gentzen <renegentzen@yahoo.de>
2024-01-14 00:25:08 +0100

    Added quiz evaluation functions and tests

--- quizgame/main.go
+++ quizgame/main.go
@@ -24,3 +24,25 @@ func NewQuestionFromRecord(record [2]string) Question {
 	}
 	return Question{record[0], converted}
 }
+
+type Quiz []Question
+
+type QuizItem struct {
+	question Question
+	answer   int
+}
+
+func (q QuizItem) Validate() bool {
+	return q.answer == q.question.answer
+}
+
+type AnsweredQuiz []QuizItem
+
+func (a AnsweredQuiz) Evaluate() (score int) {
+	for _, item := range a {
+		if item.Validate() {
+			score++
+		}
+	}
+	return score
+}
--- quizgame/main_test.go
+++ quizgame/main_test.go
@@ -37,3 +37,64 @@ func TestNewQuestionFromRecord(t *testing.T) {
 		})
 	}
 }
+
+func TestQuizItem_Validate(t *testing.T) {
+	type fields struct {
+		question Question
+		answer   int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"1", fields{Question{"", 5}, 5}, true},
+		{"2", fields{Question{"", 6}, 1}, false},
+		{"1", fields{Question{"", -1}, 10}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			q := QuizItem{
+				question: tt.fields.question,
+				answer:   tt.fields.answer,
+			}
+			if got := q.Validate(); got != tt.want {
+				t.Errorf("QuizItem.Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAnsweredQuiz_Evaluate(t *testing.T) {
+	tests := []struct {
+		name      string
+		a         AnsweredQuiz
+		wantScore int
+	}{
+		{"All correct gives full score",
+			AnsweredQuiz{
+				{Question{"", 5}, 5},
+				{Question{"", 1}, 1},
+				{Question{"", -100}, -100},
+			}, 3,
+		},
+		{"Empty quiz gives 0",
+			AnsweredQuiz{}, 0,
+		},
+		{"Mistakes deduct score",
+			AnsweredQuiz{
+				{Question{"", 1}, 5},
+				{Question{"", -2}, 2},
+				{Question{"", 0}, 0},
+			}, 1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotScore := tt.a.Evaluate(); gotScore != tt.wantScore {
+				t.Errorf("AnsweredQuiz.Evaluate() = %v, want %v", gotScore, tt.wantScore)
+			}
+		})
+	}
+}
